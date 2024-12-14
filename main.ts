20a58d8e3f8970df68e8ecde7319eec96fff2ab2
learduino <leo.paz.sira@gmail.com>
2024-12-14 16:21:25 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,18 @@
+let mySprite = sprites.create(img`
+    . . f f . f f f f f . . . . . . 
+    . f 3 3 f 3 3 3 3 3 f f . . . . 
+    f 3 3 f 3 3 3 3 3 3 3 3 f . . . 
+    f 3 3 3 3 3 3 3 3 3 3 3 3 f . . 
+    f 3 3 3 3 3 3 3 3 3 3 3 3 f . . 
+    f 3 3 3 3 3 3 3 3 3 3 3 3 3 f . 
+    f 3 3 3 3 3 3 3 3 3 3 3 3 3 3 f 
+    f 3 3 3 3 3 3 3 3 3 3 3 3 3 3 f 
+    . f 3 3 3 3 3 3 3 3 3 3 3 3 3 f 
+    . f 3 3 3 3 3 3 3 3 3 3 f f f . 
+    . f 3 3 3 3 3 3 3 3 3 f a a a f 
+    . . f 3 3 3 3 3 3 3 f a a a a f 
+    . . f f 3 3 3 3 3 3 f a a a a f 
+    . f a a f f 3 3 3 f a a a a f . 
+    f a a a a a f f f f f a a f . . 
+    . f f f f f f . . . f f f . . . 
+    `, SpriteKind.Player)
